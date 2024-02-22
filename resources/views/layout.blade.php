b0700e07180de4892d102e3d5c529de8fed07236
GregoriusDavin <karyotlol@gmail.com>
2024-02-22 11:25:53 +0700

    github push

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Database Davin</title>
+    <title>Database Davinngab</title>
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Styles -->
